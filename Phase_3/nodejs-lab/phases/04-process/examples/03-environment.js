a7669b4ea5dee2d7b04c5b6849fd1e8ab6adebab
Tamirat Tezera <tamirattezera8@gmail.com>
2026-08-10 21:34:02 +0300

    learn process environment variables

---
+++ Phase_3/nodejs-lab/phases/04-process/examples/03-environment.js
@@ -0,0 +1,14 @@
+// ==========================================================
+// NODE.JS PROCESS MODULE
+// LESSON 03 — ENVIRONMENT VARIABLES
+// ==========================================================
+//
+// process.env provides access to environment variables
+// available to the currently running Node.js process.
+//
+// ==========================================================
+
+console.log(process.env);
+console.log(process.env.HOME);
+console.log(process.env.USER);
+console.log(process.env.APP_NAME);
